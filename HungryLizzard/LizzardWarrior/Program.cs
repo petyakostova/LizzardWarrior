42e97c8ae988e4e0b36ba65978d1aa8dddcd42b4
plomba97 <plomba97@gmail.com>
2015-02-16 20:14:27 +0200

    wrote it again
    DONT BREAK IT!

--- HungryLizzard/LizzardWarrior/Program.cs
+++ HungryLizzard/LizzardWarrior/Program.cs
@@ -2,151 +2,109 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-
-namespace MaffeluDemo
+namespace HungryLizard
 {
+    class Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
     class Program
     {
-        const ConsoleColor HERO_COLOR = ConsoleColor.Gray;
-        const ConsoleColor BACKGROUND_COLOR = ConsoleColor.Gray;
+        public static Coordinate Hero { get; set; }
 
-        public static Coordinate Hero { get; set; } //Will represent our here that's moving around :P/>
+        static void PrintOnPosition(int x, int y, char c, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = color;
+            Console.Write(c);
 
-        static void Main(string[] args)
+        } 
+        
+        static void InitGame()//initializes the game and sets the Hero in the middle of the console
         {
             
-            InitGame();
-
-            ConsoleKeyInfo keyInfo;
-            while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape)
-            {
-                switch (keyInfo.Key)
-                {
-                    case ConsoleKey.RightArrow:
-                        MoveHero(Console.WindowWidth/10, 0);
-                        break;
-
-                    case ConsoleKey.LeftArrow:
-                        MoveHero(-(Console.WindowWidth/10), 0);
-                        break;
-                        
-                }
-
-            }
-        }
-
-        /// <summary>
-        /// Paint the new hero
-        /// </summary>
-        static void MoveHero(int x, int y)
-        {
-            Coordinate newHero = new Coordinate()
-            {
-                X = Hero.X + x,
-                Y = Hero.Y + y
-            };
-
-            if (CanMove(newHero))
-            {
-                
-                RemoveHero();
-                Console.Clear();
-                Console.BackgroundColor = HERO_COLOR;
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.SetCursorPosition(newHero.X, newHero.Y);
-                Console.Write(@"  __  ,          ");
-                Console.SetCursorPosition(newHero.X, newHero.Y + 1);
-                Console.Write(@" (' \ \          ");
-                Console.SetCursorPosition(newHero.X, newHero.Y + 2);
-                Console.Write(@"  \ \\/          ");
-                Console.SetCursorPosition(newHero.X, newHero.Y + 3);
-                Console.Write(@"'\/\ \\          ");
-                Console.SetCursorPosition(newHero.X, newHero.Y + 4);
-                Console.Write(@"    \ \\/\,      ");
-                Console.SetCursorPosition(newHero.X, newHero.Y + 5);
-                Console.Write(@"     \ \\        ");
-                Console.SetCursorPosition(newHero.X, newHero.Y + 6);
-                Console.Write(@"     /\ \\       ");
-                Console.SetCursorPosition(newHero.X, newHero.Y + 7);
-                Console.Write(@"     \ `\\\      ");
-                Console.SetCursorPosition(newHero.X, newHero.Y + 8);
-                Console.Write(@"     '   `\\     ");
-                Console.SetCursorPosition(newHero.X, newHero.Y + 9);
-                Console.Write(@"           \\    ");
-                Console.SetCursorPosition(newHero.X, newHero.Y + 10);
-                Console.Write(@"            `---'");
-
-                Hero = newHero;
-            }
-        }
-
-        /// <summary>
-        /// Overpaint the old hero
-        /// </summary>
-        static void RemoveHero()
-        {
-            Console.BackgroundColor = BACKGROUND_COLOR;
-            Console.SetCursorPosition(Hero.X, Hero.Y);
-            Console.Write(" ");
-        }
-
-        /// <summary>
-        /// Make sure that the new coordinate is not placed outside the
-        /// console window (since that will cause a runtime crash
-        /// </summary>
-        static bool CanMove(Coordinate c)
-        {
-            if (c.X < 0 || c.X >= Console.WindowWidth-16)
-                return false;
-
-            if (c.Y < 0 || c.Y >= Console.WindowHeight)
-                return false;
-
-            return true;
-        }
-
-        /// <summary>
-        /// Paint a background color
-        /// </summary>
-        /// <remarks>
-        /// It is very important that you run the Clear() method after
-        /// changing the background color since this causes a repaint of the background
-        /// </remarks>
-        static void SetBackgroundColor()
-        {
-            Console.BackgroundColor = BACKGROUND_COLOR;
-            Console.Clear(); //Important!
-        }
-
-        /// <summary>
-        /// Initiates the game by painting the background
-        /// and initiating the hero
-        /// </summary>
-        static void InitGame()
-        {
-            SetBackgroundColor();
 
             Hero = new Coordinate()
             {
                 X = Console.WindowWidth/2,
                 Y = Console.WindowHeight -1
             };
-
-            MoveHero(0, 0);
+            MoveHero(0);
 
         }
-   
-    }
 
-    /// <summary>
-    /// Represents a map coordinate
-    /// </summary>
-    class Coordinate
-    {
-        public int X { get; set; } //Left
-        public int Y { get; set; } //Top
+        static void MoveHero(int a)//moves the Hero, where a is number of positions its moved
+        {
+            Coordinate newHero = new Coordinate
+            {
+                X = Hero.X + a,
+                Y = Console.WindowHeight - 4
+            };
+            if (CanMove(newHero))
+            {               
+                RemoveHero();
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.SetCursorPosition(newHero.X, newHero.Y-1);
+                Console.Write("\\  /");
+                Console.SetCursorPosition(newHero.X, newHero.Y);
+                Console.Write("****");
+                Hero = newHero;
+            }
+            
+        }
+
+        static void RemoveHero()
+        {
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(Hero.X, Hero.Y);
+            Console.Write(" ");
+        }
+
+        static bool CanMove(Coordinate c)//checks if Hero is not out of borders of console
+        {
+            if (c.X>=1 && c.X<=Console.WindowWidth-5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Main
+        static void Main(string[] args) 
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.BufferHeight = Console.WindowHeight = 30;
+            Console.BufferWidth = Console.WindowWidth = 90;
+            InitGame();
+
+            while (true)
+            {
+                
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    while (Console.KeyAvailable) Console.ReadKey(true);
+                    if (keyInfo.Key == ConsoleKey.LeftArrow)
+                    {
+                        MoveHero(-1);
+                    }
+                    else if (keyInfo.Key == ConsoleKey.RightArrow)
+                    {
+                        MoveHero(1);
+                    }
+                }
+
+
+                Thread.Sleep(100);
+            }
+        }
+
     }
-    
 }
